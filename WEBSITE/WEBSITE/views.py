3e11c76973e4f917e8c12ce2d503b0e8a1caba2c
Manas <manasdeshpande@outlook.com>
2021-10-23 09:02:36 +0530

    Initial

---
+++ WEBSITE/WEBSITE/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+#username=Manas
+#Password=12341234
+def index(request):
+    return HttpResponse('This is a function based view.')
+
+def about(request):
+    return render(request, 'index.html') 
