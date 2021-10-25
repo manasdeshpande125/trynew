a31feebcde0882c648c04168455cac066a464a92
Manas <manasdeshpande@outlook.com>
2021-10-25 11:36:26 +0530

    Backend

--- WEBSITE/myapp/views.py
+++ WEBSITE/myapp/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from .models import Info
 from django.http import HttpResponse
+
 def index(request):
     return HttpResponse("Index Shop")
 def contact(request):
-    return render(request, 'index.html') 
-def info(request):
     if request.method == "POST":
 
         name = request.POST.get('name', '')
@@ -15,4 +14,18 @@ def info(request):
         
         info = Info(name=name, email=email, phone=phone, msg=msg)
         info.save()
-    return render(request, "myapp/index.html")
+        
+    return render(request, "index.html")
+    #return render(request, 'index.html') 
+'''def info(request):
+    if request.method == "POST":
+
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        msg = request.POST.get('msg', '')
+        
+        info = Info(name=name, email=email, phone=phone, msg=msg)
+        info.save()
+        print(name, email, phone, msg)
+    return render(request, "myapp/index.html")'''
